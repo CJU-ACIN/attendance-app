ecd599ad6aed85ffba43d650625962740b73f78a
wwq <wwquen3568@naver.com>
2023-07-10 14:50:13 +0900

    (fix) 설문 10문항 및 답변 최소글자 5개 삭제

--- attendance_app/survey/forms.py
+++ attendance_app/survey/forms.py
@@ -4,20 +4,66 @@ from survey.models import Survey, SurveyReply
 class SurveyForm(forms.ModelForm):
     class Meta:
         model = Survey
-        fields = ('question1', 'question2', 'question3')
+        fields = (
+            'question1', 
+            'question2', 
+            'question3',
+            'question4',
+            'question5',
+            'question6',
+            'question7',
+            'question8',
+            'question9',
+            'question10',
+            
+            )
+        
+        
         widgets = {
             'course_id': forms.Select(attrs={'class': 'form-select'}),
             'question1': forms.TextInput(attrs={'class': 'form-control'}),
             'question2': forms.TextInput(attrs={'class': 'form-control'}),
             'question3': forms.TextInput(attrs={'class': 'form-control'}),
+            'question4': forms.TextInput(attrs={'class': 'form-control'}),
+            'question5': forms.TextInput(attrs={'class': 'form-control'}),
+            'question6': forms.TextInput(attrs={'class': 'form-control'}),
+            'question7': forms.TextInput(attrs={'class': 'form-control'}),
+            'question8': forms.TextInput(attrs={'class': 'form-control'}),
+            'question9': forms.TextInput(attrs={'class': 'form-control'}),
+            'question10': forms.TextInput(attrs={'class': 'form-control'}),
+
         }
+        
+        
         labels = {
             'question1': '질문 1',
             'question2': '질문 2',
             'question3': '질문 3',
+            'question4': '질문 4',
+            'question5': '질문 5',
+            'question6': '질문 6',
+            'question7': '질문 7',
+            'question8': '질문 8',
+            'question9': '질문 9',
+            'question10': '질문 10',
         }
-
+        
+        
+# 학생 설문 응답
 class SurveyReplyForm(forms.ModelForm):
     class Meta:
         model = SurveyReply
-        fields = ('reply1', 'reply2', 'reply3')
+        fields = (
+            'reply1', 
+            'reply2', 
+            'reply3',
+            'reply4',
+            'reply5',
+            'reply6',
+            'reply7',
+            'reply8',
+            'reply9',
+            'reply10',
+                  
+            )
+
--- attendance_app/survey/models.py
+++ attendance_app/survey/models.py
@@ -7,9 +7,16 @@ from django.shortcuts import get_object_or_404
 #설문 양식
 class Survey(models.Model):
     course_id = models.OneToOneField(Course, on_delete=models.CASCADE)
-    question1 = models.CharField(max_length=100)
-    question2 = models.CharField(max_length=100, blank=True)
-    question3 = models.CharField(max_length=100, blank=True)
+    question1 = models.CharField(max_length=200)
+    question2 = models.CharField(max_length=200, blank=True)
+    question3 = models.CharField(max_length=200, blank=True)
+    question4 = models.CharField(max_length=200, blank=True)
+    question5 = models.CharField(max_length=200, blank=True)
+    question6 = models.CharField(max_length=200, blank=True)
+    question7 = models.CharField(max_length=200, blank=True)
+    question8 = models.CharField(max_length=200, blank=True)
+    question9 = models.CharField(max_length=200, blank=True)
+    question10 = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
         course = get_object_or_404(Course, pk=self.course_id.pk)
@@ -23,6 +30,13 @@ class SurveyReply(models.Model):
     reply1 = models.TextField()
     reply2 = models.TextField(blank=True)
     reply3 = models.TextField(blank=True)
+    reply4 = models.TextField(blank=True)
+    reply5 = models.TextField(blank=True)
+    reply6 = models.TextField(blank=True)
+    reply7 = models.TextField(blank=True)
+    reply8 = models.TextField(blank=True)
+    reply9 = models.TextField(blank=True)
+    reply10 = models.TextField(blank=True)
     submit_survey = models.BooleanField(default=False)
     def __str__(self):
         survey = get_object_or_404(Survey, pk=self.survey_id.pk)
